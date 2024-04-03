6a42461253c0cf33f1ac3fffa942544bacae3dca
Alfonso Chafla <117238753+Luden20@users.noreply.github.com>
2024-04-03 07:34:04 -0500

    n

--- POO/Pastel/src/Pastel.java
+++ POO/Pastel/src/Pastel.java
@@ -25,25 +25,25 @@ public class Pastel extends Applet {
 	    int ini = 0;
 	    int fin = 0;
 	    //Graphics2D para graficar
-	    Graphics2D g2d = (Graphics2D) g;    
-	    g2d.setColor(Color.RED);
+	    //Graphics2D g2d = (Graphics2D) g;    
+	    g.setColor(Color.RED);
 	    //	Calculo el angulo final en base a la prmiera posicion del arr
 	    fin = (int) (arr[0] * 360);
-	    g2d.fillArc(x, y, h, w, ini, fin);
+	    g.fillArc(x, y, h, w, ini, fin);
 	    //El angulo final e vuelve el inicial y calculo el nuevo final sumandole al anterior
 	    ini += fin; 
 	    fin = (int) (arr[1] * 360);
-	    g2d.setColor(Color.BLUE);
-	    g2d.fillArc(x, y, h, w, ini, fin);
+	    g.setColor(Color.BLUE);
+	    g.fillArc(x, y, h, w, ini, fin);
 	    ini += fin;
 	    fin = (int) (arr[2] * 360);
-	    g2d.setColor(Color.GRAY);
-	    g2d.fillArc(x, y, h, w, ini, fin);
+	    g.setColor(Color.GRAY);
+	    g.fillArc(x, y, h, w, ini, fin);
 	    ini += fin; 
 	    fin = (int) (arr[3] * 360);
-	    g2d.setColor(Color.GREEN);
-	    g2d.fillArc(x, y, h, w, ini, fin);
-	    g2d.setColor(Color.BLACK);
+	    g.setColor(Color.GREEN);
+	    g.fillArc(x, y, h, w, ini, fin);
+	    g.setColor(Color.BLACK);
 }
 
 }
