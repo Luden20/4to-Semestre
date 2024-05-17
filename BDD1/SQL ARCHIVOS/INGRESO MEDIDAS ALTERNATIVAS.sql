c80eed65f35620d16bd4964dc15b6523fadc3fbb
Luden20 <aachafla@puce.edu.ec>
2024-05-17 05:29:54 -0500

    jk

---
+++ BDD1/SQL ARCHIVOS/INGRESO MEDIDAS ALTERNATIVAS.sql
@@ -0,0 +1,320 @@
+
+-- DETERGENTE DEJA
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD001', 'LT');
+
+-- DESINFECTANTE KLM
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD002', 'LT');
+
+-- CLORO
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD003', 'LT');
+
+-- Limpiador Multiusos FreshClean
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD004', 'LT');
+
+-- Detergente en Gel ActiveMax
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD005', 'LT');
+
+-- Desinfectante de Superficies GermKill
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD006', 'LT');
+
+-- Esponja Abrasiva UltraScratch
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD007', 'U');
+
+-- Aromatizante en Spray FreshAir
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD008', 'U');
+
+-- LECHUGA CRESPA
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD009', 'U');
+
+-- PAPA CHOLA
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD010', 'U');
+
+-- Peras Jugosas
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD011', 'U');
+
+-- Uvas Frescas
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD012', 'U');
+
+-- Naranjas de Temporada
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD013', 'U');
+
+-- Pimientos Rojos
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD014', 'U');
+
+-- Tomates Cherry
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD015', 'U');
+
+-- VINO TINTO CONCHA Y TORO
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD016', 'LT');
+
+-- Gin Premium "Sapphire"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD017', 'LT');
+
+-- Brandy Reserva "Don Pedro"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD018', 'LT');
+
+-- Cerveza Artesanal "Golden Ale"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD019', 'LT');
+
+-- Vodka de Frambuesa "Raspberry Burst"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD020', 'LT');
+
+-- Tequila Blanco "El Charro"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD021', 'LT');
+
+-- Atún en Agua (Lata de 200g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD022', 'GR');
+
+-- Maíz Dulce en Conserva (Lata de 400g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD023', 'GR');
+
+-- Sardinas en Aceite de Oliva (Lata de 150g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD024', 'GR');
+
+-- Champiñones Enteros en Lata (200g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD025', 'GR');
+
+-- Duraznos en Almíbar (Lata de 250g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD026', 'GR');
+
+-- Crema Hidratante Corporal "Glowing Skin"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD029', 'LT');
+
+-- Desodorante Antitranspirante "Fresh Day"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD030', 'U');
+
+-- Jabón Líquido Antibacterial "Pure Clean"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD031', 'LT');
+
+-- Vela Aromática "Vanilla Dream"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD032', 'U');
+
+-- Almohadas de Plumón
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD033', 'U');
+
+-- Cubiertos de Acero Inoxidable (Set de 24)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD034', 'U');
+
+-- Cojín Decorativo "Boho Chic"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD035', 'U');
+
+-- Cortinas Opacas "Midnight Black"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD036', 'U');
+
+-- Camiseta de Algodón para Hombre (Talla M)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD037', 'U');
+
+-- Vestido Casual de Verano (Talla L)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD038', 'U');
+
+-- Sudadera con Capucha para Niños (Talla S)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD039', 'U');
+
+-- Pantalones Vaqueros Slim Fit para Mujer (Talla 28)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD040', 'U');
+
+-- Conjunto de Pijama de Franela (Talla XL)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD041', 'U');
+
+-- Zapatillas Deportivas para Correr (Talla 9)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD042', 'U');
+
+-- Botas de Cuero para Hombre (Talla 10)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD043', 'U');
+
+-- Sandalias de Playa para Mujer (Talla 7)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD044', 'U');
+
+-- Zapatos Formales de Cuero para Niños (Talla 4)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD045', 'U');
+
+-- Zapatillas de Casa con Forro de Felpa (Talla 8)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD046', 'U');
+
+-- Set de Bloques de Construcción (100 Piezas)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD047', 'U');
+
+-- Muñeca Interactiva con Accesorios
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD048', 'U');
+
+-- Kit de Experimentos Científicos para Niños
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD049', 'U');
+
+-- Peluche de Animal de Gran Tamaño (Oso)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD050', 'U');
+
+-- Puzzle de 1000 Piezas (Escena de Naturaleza)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD051', 'U');
+
+-- Arroz Blanco de Grano Largo (1 kg)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD052', 'KG');
+
+-- Frijoles Negros (500g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD053', 'GR');
+
+-- Lentejas Secas (750g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD054', 'GR');
+
+-- Garbanzos (400g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD055', 'GR');
+
+-- Quinoa Orgánica (300g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD056', 'GR');
+
+-- Paquete de Bolígrafos de Gel (12 unidades
+-- Paquete de Bolígrafos de Gel (12 unidades)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD057', 'U');
+
+-- Agenda Semanal de Cuero Sintético
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD058', 'U');
+
+-- Calculadora Científica de Bolsillo
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD059', 'U');
+
+-- Carpeta de Archivos de Plástico (Pack de 5)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD060', 'U');
+
+-- Papel de Copia Blanco (500 hojas)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD061', 'U');
+
+-- Analgésico para el Dolor de Cabeza (24 tabletas)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD062', 'U');
+
+-- Antiséptico para Heridas (100 ml)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD063', 'LT');
+
+-- Jarabe para la Tos (150 ml)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD064', 'LT');
+
+-- Vitaminas y Minerales (30 cápsulas)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD065', 'U');
+
+-- Termómetro Digital para Uso Oral y Axilar
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD066', 'U');
+
+-- Chocolate Negro 70% Cacao (Tableta de 100g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD067', 'GR');
+
+-- Gominolas con Sabor a Frutas (Bolsa de 200g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD068', 'GR');
+
+-- Caramelos de Menta (Bote de 150 unidades)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD069', 'U');
+
+-- Chicles de Menta Sin Azúcar (Pack de 12)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD070', 'U');
+
+-- Piruletas de Sabores Variados (Pack de 10)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD071', 'U');
+
+-- Cable HDMI de Alta Velocidad (2 metros)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD072', 'MT');
+
+-- Cable USB Tipo C a Tipo A (1 metro)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD073', 'MT');
+
+-- Cable de Carga Lightning para iPhone (2 metros)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD074', 'MT');
+
+-- Cable de Red Ethernet CAT6 (5 metros)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD075', 'MT');
+
+-- Cable de Audio Jack 3.5mm (1.5 metros)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD076', 'MT');
+
+-- Juguete Mordedor para Perros (Tamaño Grande)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD077', 'U');
+
+-- Arena para Gatos con Fragancia Lavanda (5 kg)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD078', 'KG');
+
+-- Comedero Automático para Mascotas
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD079', 'U');
+
+-- Rascador de Cartón para Gatos (Modelo Triangular)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD080', 'U');
+
+-- Juguete Interactivo para Pájaros con Campana
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD081', 'U');
+
+-- Juego de Destornilladores de Precisión (6 Piezas)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD082', 'U');
+
+-- Martillo de Carpintero de Fibra de Vidrio (500g)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD083', 'GR');
+
+-- Taladro Inalámbrico con Batería de Litio (12V)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD084', 'U');
+
+-- Sierra Circular Eléctrica (1800W)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD085', 'U');
+
+-- Juego de Llaves Allen Hexagonales (10 Piezas)
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD086', 'U');
+
+-- Contenedor de Almacenamiento
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD087', 'LT');
+
+-- Caja Organizadora de Plástico
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD088', 'U');
+
+-- Bidón para Agua
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD089', 'LT');
+
+-- Maceta de Plástico para Plantas
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD090', 'LT');
+
+-- Tupperware Hermético
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD091', 'U');
+
+-- Silla de Oficina
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD092', 'U');
+
+-- Mesa de Centro
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD093', 'U');
+
+-- Sofá de Tela
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD094', 'U');
+
+-- Cama Doble
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD095', 'U');
+
+-- Armario de Almacenaje
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD096', 'U');
+
+-- Set de Herramientas
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD097', 'U');
+
+-- Manguera de Jardín
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD098', 'MT');
+
+-- Macetero de Cerámica
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD099', 'U');
+
+-- Césped Artificial
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD100', 'MT');
+
+-- Fertilizante Orgánico
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD101', 'KG');
+
+-- Laptop Ultrabook 14"
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD102', 'U');
+
+-- Smartphone Android
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD103', 'U');
+
+-- Tableta Gráfica
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD104', 'U');
+
+-- Auriculares Inalámbricos
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD105', 'U');
+
+-- Impresora Multifuncional
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD106', 'U');
+
+-- Auriculares Inalámbricos GAMER
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD027', 'U');
+
+-- Impresora 3D
+INSERT INTO UNID_MED_ALTERNA VALUES ('CD028', 'U');
+COMMIT;
--- BDD1/SQL ARCHIVOS/INGRESO ORDEN DE COMPRA Y DETALLES (PENDIENTE DE TERMINAR).sql
+++ BDD1/SQL ARCHIVOS/INGRESO ORDEN DE COMPRA Y DETALLES (PENDIENTE DE TERMINAR).sql
@@ -1,10 +1,319 @@
 DESC DETALLE_ORDEN_COMPRA;
 DESC ORDEN_COMPRA;
-DESC KARDEX;
-SELECT * FROM PRODUCTO;
+DESC PRODUCTO_BODEGA;
+
+SELECT * FROM ORDEN_COMPRA;
+SELECT * FROM detalle_orden_compra;
+SELECT * FROM PRODUCTO_PROOVEDOR;
+SELECT * FROM PRODUCTO_BODEGA;
+
+DESC PRODUCTO_BODEGA;
+SELECT * FROM SUCURSAL;
+--S_001-S_020
+--CD001-CD106
+--BOD001-BOD020
 --EN TOTAL VA LA SUMA DE LOS PRODUCTOS DE DETALLES
 --ME TOCA HACER ESTO MANUALMENTE
 --POR LO MENOS 1 ORDEN DE COMPRA POR SUCURSAL
-INSERT INTO ORDEN_COMPRA VALUES('OC00N','BOD00N','PRV00N',F1,F2,F3,TOTAL);
-INSERT INTO DETALLE_ORDEN_COMPRA VALUES ('OC00N','');
-INSERT INTO DETALLE_ORDEN_COMPRA VALUES ('OC00N','');
+--------------------------------------------
+TRUNCATE TABLE ORDEN_COMPRA;
+TRUNCATE TABLE PRODUCTO_PROOVEDOR;
+TRUNCATE TABLE DETALLE_ORDEN_COMPRA;
+TRUNCATE TABLE PRODUCTO_PROOVEDOR;
+TRUNCATE TABLE PRODUCTO_BODEGA;
+
+
+
+INSERT INTO ORDEN_COMPRA VALUES('OC001','BOD001','PRV001','2023-01-01','2023-01-15','2023-01-26',TOTAL);
+INSERT INTO DETALLE_ORDEN_COMPRA VALUES ('OC001','CD001');
+INSERT INTO DETALLE_ORDEN_COMPRA VALUES ('OC001','CD002');
+INSERT INTO DETALLE_ORDEN_COMPRA VALUES ('OC001','CD003');
+INSERT INTO PRODUCTO_PROOVEDOR VALUES('CD001','PRV001');
+INSERT INTO PRODUCTO_PROOVEDOR VALUES('CD002','PRV001');
+INSERT INTO PRODUCTO_PROOVEDOR VALUES('CD003','PRV001');
+INSERT INTO PRODUCTO_BODEGA VALUES('CD001','BOD001');
+INSERT INTO PRODUCTO_BODEGA VALUES('CD002','BOD001');
+INSERT INTO PRODUCTO_BODEGA VALUES('CD003','BOD001');
+
+-----
+ALTER TABLE ORDEN_COMPRA MODIFY(ORD_ELABORO VARCHAR2(25));
+----------------------------------------------
+CREATE OR REPLACE PROCEDURE GENERAR_ORDEN_COMPRA (
+    p_orden_numero IN VARCHAR2,
+    p_bodega_codigo IN VARCHAR2,
+    p_proveedor_codigo IN VARCHAR2,
+    p_fecha_inicio IN DATE,
+    p_fecha_entrega IN DATE,
+    p_elaboro IN VARCHAR2, -- Cambiado a p_elaboro
+    p_productos IN SYS.ODCIVARCHAR2LIST,
+    p_total OUT NUMBER
+)
+IS
+    v_total NUMBER := 0;
+BEGIN
+    -- Insertar en ORDEN_COMPRA
+    INSERT INTO ORDEN_COMPRA (ORD_NUMERO__, BOD_CODIGO, PRV_CODIGO__, ORD_FECHA, ORD_FECHA_LIM, ORD_ELABORO, ORD_TOTAL)
+    VALUES (p_orden_numero, p_bodega_codigo, p_proveedor_codigo, p_fecha_inicio, p_fecha_entrega, p_elaboro, v_total) -- Cambiado a p_elaboro
+    RETURNING ORD_TOTAL INTO p_total;
+
+    -- Insertar en DETALLE_ORDEN_COMPRA y PRODUCTO_PROVEEDOR
+    FOR i IN 1..p_productos.COUNT LOOP
+        INSERT INTO DETALLE_ORDEN_COMPRA (ORD_NUMERO__, PRD_CODIGO__)
+        VALUES (p_orden_numero, p_productos(i));
+
+        -- Insertar en PRODUCTO_PROVEEDOR si no existe ya
+        BEGIN
+            INSERT INTO PRODUCTO_PROOVEDOR (PRD_CODIGO__, PRV_CODIGO__)
+            VALUES (p_productos(i), p_proveedor_codigo);
+        EXCEPTION
+            WHEN DUP_VAL_ON_INDEX THEN
+                NULL; -- Ignorar si ya existe el registro
+        END;
+
+        -- Actualizar el total de la orden
+        SELECT SUM(PRD_PRECIO__) INTO v_total
+        FROM PRODUCTO
+        WHERE PRD_CODIGO__ IN (SELECT PRD_CODIGO__ FROM DETALLE_ORDEN_COMPRA WHERE ORD_NUMERO__ = p_orden_numero);
+
+        UPDATE ORDEN_COMPRA
+        SET ORD_TOTAL = v_total
+        WHERE ORD_NUMERO__ = p_orden_numero;
+
+        -- Insertar en PRODUCTO_BODEGA si no existe ya
+        BEGIN
+            INSERT INTO PRODUCTO_BODEGA (BOD_CODIGO, PRD_CODIGO__)
+            VALUES (p_bodega_codigo, p_productos(i));
+        EXCEPTION
+            WHEN DUP_VAL_ON_INDEX THEN
+                NULL; -- Ignorar si ya existe el registro
+        END;
+    END LOOP;
+
+    p_total := v_total;
+END;
+/
+
+
+DECLARE
+    v_total_orden NUMBER;
+BEGIN
+    -- Llamados al procedimiento GENERAR_ORDEN_COMPRA desde OC001 hasta OC020
+    GENERAR_ORDEN_COMPRA (
+        'OC001', -- Código de orden de compra
+        'BOD001',
+        'PRV001', -- Código del proveedor
+        TO_DATE('2023-01-01', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-01-15', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Juan Perez', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD001', 'CD002', 'CD003'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+
+    GENERAR_ORDEN_COMPRA (
+        'OC002', -- Código de orden de compra
+        'BOD002',
+        'PRV002', -- Código del proveedor
+        TO_DATE('2023-01-16', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-01-31', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Maria Garcia', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD004', 'CD005', 'CD006'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+
+    GENERAR_ORDEN_COMPRA (
+        'OC003', -- Código de orden de compra
+        'BOD003',
+        'PRV003', -- Código del proveedor
+        TO_DATE('2023-02-01', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-02-15', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Pedro Martinez', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD007', 'CD008', 'CD009'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+
+    GENERAR_ORDEN_COMPRA (
+        'OC004', -- Código de orden de compra
+        'BOD004',
+        'PRV004', -- Código del proveedor
+        TO_DATE('2023-02-16', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-02-28', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Ana Lopez', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD010', 'CD011', 'CD012'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+
+    GENERAR_ORDEN_COMPRA (
+        'OC005', -- Código de orden de compra
+        'BOD005',
+        'PRV005', -- Código del proveedor
+        TO_DATE('2023-03-01', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-03-15', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Laura Ramirez', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD013', 'CD014', 'CD015'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+
+    GENERAR_ORDEN_COMPRA (
+        'OC006', -- Código de orden de compra
+        'BOD006',
+        'PRV006', -- Código del proveedor
+        TO_DATE('2023-03-16', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-03-31', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Carlos Sanchez', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD016', 'CD017', 'CD018'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+
+    GENERAR_ORDEN_COMPRA (
+        'OC007', -- Código de orden de compra
+        'BOD007',
+        'PRV007', -- Código del proveedor
+        TO_DATE('2023-04-01', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-04-15', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Sofia Fernandez', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD019', 'CD020', 'CD021'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+
+    GENERAR_ORDEN_COMPRA (
+        'OC008', -- Código de orden de compra
+        'BOD008',
+        'PRV008', -- Código del proveedor
+        TO_DATE('2023-04-16', 'YYYY-MM-DD'), -- Fecha de inicio
+        TO_DATE('2023-04-30', 'YYYY-MM-DD'), -- Fecha de entrega
+        'Diego Gomez', -- Nombre del elaborador
+        SYS.ODCIVARCHAR2LIST('CD022', 'CD023', 'CD024'), -- Lista de códigos de productos
+        v_total_orden -- Salida para el total de la orden
+    );
+    GENERAR_ORDEN_COMPRA (
+    'OC009', -- Código de orden de compra
+    'BOD009',
+    'PRV009', -- Código del proveedor
+    TO_DATE('2023-05-01', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-05-15', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Elena Rodriguez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD025', 'CD026', 'CD027'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC010', -- Código de orden de compra
+    'BOD010',
+    'PRV010', -- Código del proveedor
+    TO_DATE('2023-05-16', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-05-31', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Fernando Martinez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD028', 'CD029', 'CD030'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC011', -- Código de orden de compra
+    'BOD011',
+    'PRV011', -- Código del proveedor
+    TO_DATE('2023-06-01', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-06-15', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Marta Ruiz', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD031', 'CD032', 'CD033'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC012', -- Código de orden de compra
+    'BOD012',
+    'PRV012', -- Código del proveedor
+    TO_DATE('2023-06-16', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-06-30', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Jose Perez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD034', 'CD035', 'CD036'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC013', -- Código de orden de compra
+    'BOD013',
+    'PRV013', -- Código del proveedor
+    TO_DATE('2023-07-01', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-07-15', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Ana Fernandez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD037', 'CD038', 'CD039'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC014', -- Código de orden de compra
+    'BOD014',
+    'PRV014', -- Código del proveedor
+    TO_DATE('2023-07-16', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-07-31', 'YYYY-MM-DD'), -- Fecha de entrega
+    'David Lopez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD040', 'CD041', 'CD042'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC015', -- Código de orden de compra
+    'BOD015',
+    'PRV015', -- Código del proveedor
+    TO_DATE('2023-08-01', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-08-15', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Elena Sanchez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD043', 'CD044', 'CD045'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC016', -- Código de orden de compra
+    'BOD016',
+    'PRV016', -- Código del proveedor
+    TO_DATE('2023-08-16', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-08-31', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Santiago Rodriguez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD046', 'CD047', 'CD048'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC017', -- Código de orden de compra
+    'BOD017',
+    'PRV017', -- Código del proveedor
+    TO_DATE('2023-09-01', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-09-15', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Laura Martinez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD049', 'CD050', 'CD051'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC018', -- Código de orden de compra
+    'BOD018',
+    'PRV018', -- Código del proveedor
+    TO_DATE('2023-09-16', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-09-30', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Carlos Fernandez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD052', 'CD053', 'CD054'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+
+GENERAR_ORDEN_COMPRA (
+    'OC019', -- Código de orden de compra
+    'BOD019',
+    'PRV019', -- Código del proveedor
+    TO_DATE('2023-10-01', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-10-15', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Martina Gomez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD055', 'CD056', 'CD057'),
+    v_total_orden -- Salida para el total de la orden
+);
+GENERAR_ORDEN_COMPRA (
+    'OC020', -- Código de orden de compra
+    'BOD020',
+    'PRV020', -- Código del proveedor
+    TO_DATE('2023-10-16', 'YYYY-MM-DD'), -- Fecha de inicio
+    TO_DATE('2023-10-31', 'YYYY-MM-DD'), -- Fecha de entrega
+    'Alejandro Perez', -- Nombre del elaborador
+    SYS.ODCIVARCHAR2LIST('CD058', 'CD059', 'CD060'), -- Lista de códigos de productos
+    v_total_orden -- Salida para el total de la orden
+);
+END;
+/
